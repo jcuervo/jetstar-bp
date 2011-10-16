c12165d3f9f3ff1ca3868b47d5b83817914f8d8d
Jimmy Cuervo <richard.gonzales@gmail.com>
2011-10-17 07:44:36 +0800

    first payload

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Jetbp::Application.config.secret_token = '9ee56e2c6898634b0c6375d5ca2e6f1e4b81e7c21efcc7985a55c7b0d8660575e695a66a1a9ce09defd58d09e7af29b6fd21259d41eca76fa5a02323e45fdf33'
