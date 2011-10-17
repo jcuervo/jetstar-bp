036f38296cb52e9156c498b40267f4395ec46a57
honelyn go <bchan@bchans-Mac-mini.local>
2011-10-17 12:45:23 +0800

    borga

--- app/controllers/flight_controller.rb
+++ app/controllers/flight_controller.rb
@@ -8,6 +8,10 @@ class FlightController < ApplicationController
     session[:dest] = params[:to] if params[:to]
     session[:depart] = params[:depart] if params[:depart]
     session[:return] = params[:return] if params[:return]
+    
+    if params[:commit] == "Find Exact Date Flights"
+      findExactFlights
+    end
 
     redirect_to flight_index_path
   end
@@ -74,4 +78,35 @@ class FlightController < ApplicationController
     
     airports
   end
+  
+  def findExactFlights
+    @flights = []
+    if session[:depart]
+
+      o = session[:origin][-4..-2]
+      d = session[:dest][-4..-2]
+      date = session[:depart].split('/')
+      rDate = "#{date[2]}#{date[0]}#{date[1]}"
+                        
+      url = URI.parse("http://110.232.117.57:8080/JetstarWebServices/services/flights/exactDates/#{o}/#{d}/#{rDate}/#{params[:adults]}/#{params[:child]}/#{params[:infants]}")
+      req = Net::HTTP::Get.new(url.path)
+      res = Net::HTTP.start(url.host, url.port) {|http|
+        http.request(req)
+      }
+      parsed_json = ActiveSupport::JSON.decode(res.body)
+      parsed_json["wrapper"]["results"].each do |flight|
+#{"arrivalAirport":"AVV","arrivalDateTime":"2011-10-21T09:15:00+11:00","businessClassAvailable":false,"carrierCode":"JQ","currency":"AUD","departureAirport":"SYD","departureDateTime":"2011-10-21T07:40:00+11:00","flightDesignator":"JQ 625","flightNumber":" 625","numStops":1,"opSuffix":" ","price":39}
+
+        @flights << ["#{flight["currency"]} #{flight["price"]}, #{flight["departureAirport"]} (#{flight["departureDateTime"]}), #{flight["arrivalAirport"]} #{flight["arrivalDateTime"]}"]
+      end if parsed_json["wrapper"]["results"]
+    end
+
+  end
+  
+  def findFlexiFlights
+  end
+  
+  def show
+  end
+
 end
