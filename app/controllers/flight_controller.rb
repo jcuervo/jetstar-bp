c856ec8874af4cb31281ddbd62986a8e0699c83a
honelyn go <bchan@bchans-Mac-mini.local>
2011-10-17 13:14:37 +0800

    find flights

--- app/controllers/flight_controller.rb
+++ app/controllers/flight_controller.rb
@@ -17,6 +17,8 @@ class FlightController < ApplicationController
       
       findFlights
     end
+        
+    redirect_to flight_index_path
   end
   
   def search
@@ -92,13 +94,42 @@ class FlightController < ApplicationController
       parsed_json["wrapper"]["results"].each do |flight|
 #{"arrivalAirport":"AVV","arrivalDateTime":"2011-10-21T09:15:00+11:00","businessClassAvailable":false,"carrierCode":"JQ","currency":"AUD","departureAirport":"SYD","departureDateTime":"2011-10-21T07:40:00+11:00","flightDesignator":"JQ 625","flightNumber":" 625","numStops":1,"opSuffix":" ","price":39}
 
-        @flights << ["#{flight["currency"]} #{flight["price"]}, #{flight["departureAirport"]} (#{flight["departureDateTime"]}), #{flight["arrivalAirport"]} #{flight["arrivalDateTime"]}"]
+        @flights << ["#{flight["currency"]} #{flight["price"]}, #{flight["departureAirport"]} #{flight["departureDateTime"]}, #{flight["arrivalAirport"]} #{flight["arrivalDateTime"]}"]
       end if parsed_json["wrapper"]["results"]
     end
 
   end
 
-  def show
+  
+  def findFlights
+    @flights = []
+    if session[:depart]
+
+      o = session[:origin][-4..-2]
+      d = session[:dest][-4..-2]
+      date = session[:depart].split('/')
+      rDate = "#{date[2]}#{date[0]}#{date[1]}"
+
+      if params[:commit].downcase.index("exact")
+        url = URI.parse("http://110.232.117.57:8080/JetstarWebServices/services/flights/exactDates/#{o}/#{d}/#{rDate}/#{session[:adults]}/#{session[:child]}/#{session[:infants]}")
+      else
+        url = URI.parse("http://110.232.117.57:8080/JetstarWebServices/services/flights/flexibletDates/#{o}/#{d}/#{rDate}/#{session[:adults]}/#{session[:child]}/#{session[:infants]}")
+      end
+      
+      req = Net::HTTP::Get.new(url.path)
+      res = Net::HTTP.start(url.host, url.port) {|http|
+        http.request(req)
+      }
+      parsed_json = ActiveSupport::JSON.decode(res.body)
+      parsed_json["wrapper"]["results"].each do |flight|
+
+        ddt = flight["departureDateTime"].split('T')
+        adt = flight["departureDateTime"].split('T')
+
+        @flights << ["#{flight["currency"]} #{flight["price"]}, #{flight["departureAirport"]} #{ddt[0]} (#{ddt[1]}), #{flight["arrivalAirport"]} #{adt[0]} (#{adt[1]})"]
+        
+      end if parsed_json["wrapper"]["results"]
+    end
   end
 
 end
